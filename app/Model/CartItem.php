42c6c417b39ca3f25b8ff3d919d8d23fabef5770
Jan Kowalski <adrimerie@interia.pl>
2017-07-18 12:50:46 +0200

    wypieklem model dla koszyka

---
+++ app/Model/CartItem.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * CartItem Model
+ *
+ */
+class CartItem extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'item_name';
+
+}
---
+++ app/Test/Fixture/CartItemFixture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * CartItem Fixture
+ */
+class CartItemFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'item_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+		'item_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'price' => array('type' => 'float', 'null' => false, 'default' => null, 'unsigned' => false),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'item_id' => 1,
+			'item_name' => 'Lorem ipsum dolor sit amet',
+			'price' => 1
+		),
+	);
+
+}
